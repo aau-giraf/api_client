938ceb9f35d89e431fcaad7f9ead86869581e46e
Mads Stenkær <mstenk16@student.aau.dk>
2019-05-08 14:36:46 +0200

    Added timer model

--- lib/models/activity_model.dart
+++ lib/models/activity_model.dart
@@ -1,17 +1,23 @@
-import 'package:api_client/models/enums/activity_state_enum.dart';
 import 'package:api_client/models/model.dart';
 import 'package:api_client/models/pictogram_model.dart';
+import 'package:api_client/models/timer_model.dart';
 import 'package:meta/meta.dart';
 
+import 'enums/activity_state_enum.dart';
+
+/// The model for the activity in the api client.
 class ActivityModel implements Model {
+  /// Constructor for Activity
   ActivityModel({
     @required this.id,
     @required this.pictogram,
     @required this.order,
     @required this.state,
     @required this.isChoiceBoard,
+    this.timer
   });
 
+  /// Constructs the activityModel from json.
   ActivityModel.fromJson(Map<String, dynamic> json) {
     if (json == null) {
       throw const FormatException(
@@ -23,10 +29,14 @@ class ActivityModel implements Model {
     order = json['order'];
     state = ActivityState.values[(json['state']) - 1];
     isChoiceBoard = json['isChoiceBoard'];
+    //TODO SHOULD BE test etstest UNCOMMENTED WHEN BACKEND IS READY
+    // timer = TimerModel.fromJson(json['timer']);
   }
 
+  /// The ID of the activity.
   int id;
 
+  /// The pictogram for the activity.
   PictogramModel pictogram;
 
   /// The order that the activity will appear on in a weekschedule. If two has
@@ -40,6 +50,9 @@ class ActivityModel implements Model {
   /// never be set from our side
   bool isChoiceBoard;
 
+  /// The timer for the activity
+  TimerModel timer;
+
   @override
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
@@ -48,6 +61,8 @@ class ActivityModel implements Model {
       'order': order,
       'state': state.index + 1,
       'isChoiceBoard': isChoiceBoard,
+      //TODO SHOULD BE UNCOMMENTED WHEN BACKEND IS READY
+      //'timer' : timer.toJson()
     };
   }
 }
---
+++ lib/models/timer_model.dart
@@ -0,0 +1,53 @@
+import 'package:api_client/models/model.dart';
+import 'package:meta/meta.dart';
+
+/// Represents a timer for an activity
+class TimerModel implements Model {
+  /// Constructor for timer
+  TimerModel({
+    @required this.startTime,
+    @required this.progress,
+    @required this.fullLength,
+    @required this.paused,
+  });
+
+  /// Constructor for the timer from json.
+  TimerModel.fromJson(Map<String, dynamic> json) {
+    if (json == null){
+      throw const FormatException(
+          '[ActivityModel]: Cannot initialize from null');
+      startTime = null;
+      progress = null;
+      fullLength = null;
+      paused = null;
+    } else {
+      startTime = json['startTimer'];
+      progress = json['prgress'];
+      fullLength = json['fullLength'];
+      paused = json['paused'];
+    }
+  }
+
+  /// The time for when the timer started.
+  DateTime startTime;
+
+  /// The progress of the timer
+  int progress;
+
+  /// The full timer length
+  int fullLength;
+
+  /// Bool if the timer is paused or not
+  bool paused;
+
+
+  @override
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'startTimer': startTime,
+      'progress': progress,
+      'fullLength': fullLength,
+      'paused': paused
+    };
+  }
+}
