de889c7db76bc93afc2ccd226141f6f05c252337
swuf <swuf@users.noreply.github.com>
2019-05-14 14:17:15 +0200

    Create Add and Delete activity endpoints.

--- test/api/activity_api_test.dart
+++ test/api/activity_api_test.dart
@@ -2,8 +2,11 @@ import 'package:api_client/api/activity_api.dart';
 import 'package:api_client/models/activity_model.dart';
 import 'package:api_client/models/enums/access_level_enum.dart';
 import 'package:api_client/models/enums/activity_state_enum.dart';
+import 'package:api_client/models/enums/weekday_enum.dart';
 import 'package:api_client/models/pictogram_model.dart';
 import 'package:api_client/models/username_model.dart';
+import 'package:api_client/models/week_model.dart';
+import 'package:api_client/models/weekday_model.dart';
 import 'package:test_api/test_api.dart';
 import 'package:api_client/http/http_mock.dart';
 
@@ -13,18 +16,29 @@ void main() {
   final UsernameModel mockUser =
       UsernameModel(id: '1', name: 'Test', role: 'Guardian');
 
+  final PictogramModel mockPictogram = PictogramModel(
+      id: 69,
+      title: 'hi',
+      accessLevel: AccessLevel.PUBLIC,
+      lastEdit: DateTime.now(),
+      imageHash: null,
+      imageUrl: null);
+
   final ActivityModel mockActivity = ActivityModel(
       id: 1,
       state: ActivityState.Normal,
       isChoiceBoard: false,
       order: 0,
-      pictogram: PictogramModel(
-          id: 69,
-          title: 'hi',
-          accessLevel: AccessLevel.PUBLIC,
-          lastEdit: DateTime.now(),
-          imageHash: null,
-          imageUrl: null));
+      pictogram: mockPictogram);
+
+  final WeekModel mockWeek = WeekModel(
+      thumbnail: mockPictogram,
+      name: 'TestWeek',
+      weekYear: 1999,
+      weekNumber: 42,
+      days: <WeekdayModel>[
+        WeekdayModel(day: Weekday.Sunday, activities: <ActivityModel>[])
+      ]);
 
   setUp(() {
     httpMock = HttpMock();
@@ -42,6 +56,45 @@ void main() {
         .expectOne(url: '/${mockUser.id}/update', method: Method.patch)
         .flush(<String, dynamic>{
       'data': mockActivity.toJson(),
+      'success': true,
+      'errorProperties': <dynamic>[],
+      'errorKey': 'NoError',
+    });
+  });
+
+  test('Should add an activity', () {
+    activityApi
+        .add(mockActivity, mockUser.id, mockWeek.name, mockWeek.weekYear,
+            mockWeek.weekNumber, mockWeek.days.first.day)
+        .listen(expectAsync1((ActivityModel response) {
+      expect(response.toJson(), mockActivity.toJson());
+    }));
+
+    httpMock
+        .expectOne(
+            url: '/${mockUser.id}/${mockWeek.name}/${mockWeek.weekYear}/'
+                '${mockWeek.weekNumber}/${mockWeek.days.first.day.index + 1}',
+            method: Method.post)
+        .flush(<String, dynamic>{
+      'data': mockActivity.toJson(),
+      'success': true,
+      'errorProperties': <dynamic>[],
+      'errorKey': 'NoError',
+    });
+  });
+
+  test('Should delete an activity', () {
+    activityApi
+        .delete(mockActivity.id, mockUser.id)
+        .listen(expectAsync1((bool response) {
+      expect(response, true);
+    }));
+
+    httpMock
+        .expectOne(
+            url: '/${mockUser.id}/delete/${mockActivity.id}',
+            method: Method.delete)
+        .flush(<String, dynamic>{
       'success': true,
       'errorProperties': <dynamic>[],
       'errorKey': 'NoError',
