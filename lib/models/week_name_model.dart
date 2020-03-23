1d90e897843c816abdb3dddaa73fb78c66ae13e5
HaxorHurley <33719181+HaxorHurley@users.noreply.github.com>
2020-03-23 14:10:11 +0100

    Linter update

--- lib/models/week_name_model.dart
+++ lib/models/week_name_model.dart
@@ -28,7 +28,7 @@ class WeekNameModel implements Model {
   int weekYear;
 
   /// The number of the week, 0 - 52 (53). 
-  /// If the year starts on a Thursday or is a leap year that starts on a Wednesday,
+  /// If the year starts on a Thursday or is a leap year,
   /// that  year will have 53 numbered weeks.
   int weekNumber;
 
