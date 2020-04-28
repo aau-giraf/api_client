be8026c138caa2bd7e8ba1d20eb33928bc2383f0
TobiasNielsen <34129506+TobiasNielsen@users.noreply.github.com>
2020-04-28 09:08:01 +0200

    fixed error

--- lib/offline_repository/db_model_factory.dart
+++ lib/offline_repository/db_model_factory.dart
@@ -1,14 +1,14 @@
 import 'package:api_client/models/activity_model.dart';
+import 'package:api_client/models/displayname_model.dart';
 import 'package:api_client/models/model.dart';
-import 'package:api_client/models/username_model.dart';
 
 /// Model factory for the database
 class ModelFactory {
 
   /// Method for getting correct model
   static Model getModel(Map<String, dynamic> json, String objectName) {
-    if (objectName == (UsernameModel).toString()) {
-      return UsernameModel.fromJson(json);
+    if (objectName == (DisplayNameModel).toString()) {
+      return DisplayNameModel.fromJson(json);
     } else if (objectName == (ActivityModel).toString()) {
       return ActivityModel.fromJson(json);
     }
