e1b3400c02485759c6e24c892d67d9ab6b259ca1
Christopher Kirkegaard Sørensen <32838871+ChrisKS@users.noreply.github.com>
2020-05-15 09:21:35 +0200

    Ran rxdart_codemod and updated the dependency to use rxdart: ^0.24.0 (#63)

--- lib/api/user_api.dart
+++ lib/api/user_api.dart
@@ -1,7 +1,6 @@
 import 'package:api_client/api/api_exception.dart';
 import 'package:api_client/http/http.dart';
 import 'package:flutter/material.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:api_client/models/giraf_user_model.dart';
 import 'package:api_client/models/settings_model.dart';
 import 'package:api_client/models/displayname_model.dart';
@@ -14,7 +13,7 @@ class UserApi {
   final Http _http;
 
   /// Find information about the currently authenticated user.
-  Observable<GirafUserModel> me() {
+  Stream<GirafUserModel> me() {
     return _http
         .get('/')
         .map((Response res) => GirafUserModel.fromJson(res.json['data']));
@@ -23,7 +22,7 @@ class UserApi {
   /// Find information on the user with the given ID
   ///
   /// [id] ID of the user
-  Observable<GirafUserModel> get(String id) {
+  Stream<GirafUserModel> get(String id) {
     return _http
         .get('/$id')
         .map((Response res) => GirafUserModel.fromJson(res.json['data']));
@@ -32,7 +31,7 @@ class UserApi {
   /// Updates the user with the information in GirafUserModel
   ///
   /// [user] The updated user
-  Observable<GirafUserModel> update(GirafUserModel user) {
+  Stream<GirafUserModel> update(GirafUserModel user) {
     return _http
         .put('/${user.id}', user.toJson())
         .map((Response res) => GirafUserModel.fromJson(res.json['data']));
@@ -41,7 +40,7 @@ class UserApi {
   /// Get user-settings for the user with the specified Id
   ///
   /// [id] Identifier of the GirafUser to get settings for
-  Observable<SettingsModel> getSettings(String id) {
+  Stream<SettingsModel> getSettings(String id) {
     return _http
         .get('/$id/settings')
         .map((Response res){
@@ -56,7 +55,7 @@ class UserApi {
   ///
   /// [id] Identifier of the GirafUser to update settings for
   /// [settings] reference to a Settings containing the new settings
-  Observable<SettingsModel> updateSettings(String id, SettingsModel settings) {
+  Stream<SettingsModel> updateSettings(String id, SettingsModel settings) {
     return _http
         .put('/$id/settings', settings.toJson())
         .map((Response res){
@@ -70,20 +69,21 @@ class UserApi {
   /// Deletes the user icon for a given user
   ///
   /// [id] Identifier fo the user to which the icon should be deleted
-  Observable<bool> deleteIcon(String id) {
+  Stream<bool> deleteIcon(String id) {
     return _http.delete('/$id/icon').map((Response res) => res.statusCode() == 200);
   }
 
   /// Gets the raw user icon for a given user
   ///
   /// [id] Identifier of the GirafUser to get icon for
-  Observable<Image> getIcon(String id) {
+  Stream<Image> getIcon(String id) {
     return _http.get('/$id/icon/raw').map((Response res) {
       return Image.memory(res.response.bodyBytes);
     });
   }
 
-  Observable<bool> updateIcon() {
+  /// TO DO(boginw): implement this
+  Stream<bool> updateIcon() {
     // TODO(boginw): implement this
     return null;
   }
@@ -92,7 +92,7 @@ class UserApi {
   /// be a guardian
   ///
   /// [id] Identifier of the GirafUser to get citizens for
-  Observable<List<DisplayNameModel>> getCitizens(String id) {
+  Stream<List<DisplayNameModel>> getCitizens(String id) {
     return _http.get('/$id/citizens').map((Response res) {
       if (res.json['data'] is List) {
         return List<Map<String, dynamic>>.from(res.json['data'])
@@ -108,7 +108,7 @@ class UserApi {
   /// provided id.
   ///
   /// [id] Identifier for the citizen to get guardians for
-  Observable<List<DisplayNameModel>> getGuardians(String id) {
+  Stream<List<DisplayNameModel>> getGuardians(String id) {
     return _http.get('/$id/guardians').map((Response res) {
       if (res.json['data'] is List) {
         return List<Map<String, dynamic>>.from(res.json['data'])
@@ -125,7 +125,7 @@ class UserApi {
   ///
   /// [guardianId] The guardian
   /// [citizenId] The citizen to be added to the guardian
-  Observable<bool> addCitizenToGuardian(String guardianId, String citizenId) {
+  Stream<bool> addCitizenToGuardian(String guardianId, String citizenId) {
     return _http
         .post('/$guardianId/citizens/$citizenId')
         .map((Response res) => res.statusCode() == 200);
