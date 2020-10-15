85cc2b9b069d10470d13e8bd69ebab15464e1732
Riasalit <daniel-vilslev@hotmail.com>
2020-10-15 12:34:53 +0200

    We can now create a database

--- lib/offline_database/offline_db_handler.dart
+++ lib/offline_database/offline_db_handler.dart
@@ -1,121 +1,110 @@
-import 'dart:io';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqlite3/sqlite3.dart' as sqlite3;
+import 'package:sqflite_common/sqlite_api.dart';
 
 /// OfflineDbHandler is used for communication with the offline database
 class OfflineDbHandler {
   ///Constructor for the dbhandler
-  OfflineDbHandler() {
-    _database = initDB();
+  OfflineDbHandler(Database db) {
+    _database = db;
   }
-  Future<Database> _database;
+  Database _database;
 
   /// Initiate the database
-  Future<Database> initDB() async {
-    return getDatabasesPath().then((String path) {
-      return openDatabase(join(path, 'offlineGiraf.db'));
-    });
-  }
 
   ///Creates all of the tables in the DB
-  Future<void> createTables() async {
-    final Database database = await _database;
-    while (database == null) {}
-    database.transaction((Transaction txn) async {
-      await txn.execute('CREATE TABLE IF NOT EXISTS `Users` ('
-          '`Id` varchar( 255 ) NOT NULL, '
-          '`Role` varchar ( 255 ) NOT NULL, '
-          '`RoleName` varchar ( 256 ) DEFAULT NULL, '
-          '`Username` varchar ( 256 ) DEFAULT NULL, '
-          '`DisplayName` longtext NOT NULL, '
-          '`Department` integer DEFAULT NULL, '
-          'UNIQUE(`Id`,`UserName`), '
-          'PRIMARY KEY(`Id`));');
-      await txn.execute('CREATE TABLE IF NOT EXISTS `GuardianRelations` ('
-          '`Id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
-          '`CitizenId`	varchar ( 255 ) NOT NULL, '
-          '`GuardianId`	varchar ( 255 ) NOT NULL, '
-          'CONSTRAINT `FK_GuardianRelations_Users_CitizenId` '
-          'FOREIGN KEY(`CitizenId`) '
-          'REFERENCES `AspNetUsers`(`Id`) ON DELETE CASCADE, '
-          'CONSTRAINT `FK_GuardianRelations_Users_GuardianId` '
-          'FOREIGN KEY(`GuardianId`) '
-          'REFERENCES `AspNetUsers`(`Id`) ON DELETE CASCADE);');
-      await txn.execute('CREATE TABLE IF NOT EXISTS `WeekTemplates` ('
-          '`id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
-          '`Name`	longtext COLLATE BINARY, '
-          '`ThumbnailKey`	integer NOT NULL, '
-          'CONSTRAINT `FK_WeekTemplates_Pictograms_ThumbnailKey` '
-          'FOREIGN KEY(`ThumbnailKey`) '
-          'REFERENCES `Pictograms`(`id`) ON DELETE CASCADE);');
-      await txn.execute('CREATE TABLE IF NOT EXISTS `Weeks` ('
-          '`id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, '
-          '`GirafUserId`	varchar ( 255 ) NOT NULL, '
-          '`Name`	longtext COLLATE BINARY, '
-          '`ThumbnailKey`	integer NOT NULL, '
-          '`WeekNumber`	integer NOT NULL, '
-          '`WeekYear`	integer NOT NULL,'
-          'CONSTRAINT `FK_Weeks_AspNetUsers_GirafUserId` '
-          'FOREIGN KEY(`GirafUserId`) '
-          'REFERENCES `AspNetUsers`(`Id`) ON DELETE CASCADE,'
-          'CONSTRAINT `FK_Weeks_Pictograms_ThumbnailKey` '
-          'FOREIGN KEY(`ThumbnailKey`) '
-          'REFERENCES `Pictograms`(`id`) ON DELETE CASCADE);');
-      await txn.execute('CREATE TABLE IF NOT EXISTS `Weekdays` ('
-          '`id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, '
-          '`Day`	integer NOT NULL, '
-          '`WeekId`	integer DEFAULT NULL, '
-          '`WeekTemplateId`	integer DEFAULT NULL,'
-          'CONSTRAINT `FK_Weekdays_WeekTemplates_WeekTemplateId` '
-          'FOREIGN KEY(`WeekTemplateId`) '
-          'REFERENCES `WeekTemplates`(`id`) ON DELETE CASCADE,'
-          'CONSTRAINT `FK_Weekdays_Weeks_WeekId` '
-          'FOREIGN KEY(`WeekId`) '
-          'REFERENCES `Weeks`(`id`) ON DELETE CASCADE);');
-      await txn.execute('CREATE TABLE IF NO EXISTS `Pictograms` ('
-          '`id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
-          '`AccessLevel`	integer NOT NULL, '
-          '`LastEdit`	datetime ( 6 ) NOT NULL, '
-          '`Sound`	longblob, '
-          '`Title`	varchar ( 255 ) NOT NULL, '
-          '`ImageHash`	longtext COLLATE BINARY,'
-          'UNIQUE(`id`,`Title`));');
-      await txn.execute('CREATE TABLE IF NOT EXISTS `Activities` ('
-          '`Key`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
-          '`Order`	integer NOT NULL, '
-          '`OtherKey`	integer NOT NULL, '
-          '`State`	integer NOT NULL, '
-          '`TimerKey`	integer DEFAULT NULL, '
-          '`IsChoiceBoard`	integer NOT NULL DEFAULT \'0\', '
-          'CONSTRAINT `FK_Activities_Timers_TimerKey` '
-          'FOREIGN KEY(`TimerKey`) '
-          'REFERENCES `Timers`(`Key`) ON DELETE SET NULL,'
-          'CONSTRAINT `FK_Activities_Weekdays_OtherKey` '
-          'FOREIGN KEY(`OtherKey`) '
-          'REFERENCES `Weekdays`(`id`) ON DELETE CASCADE);');
-      await txn.execute('CREATE TABLE IF NOT EXISTS `PictogramRelations` ('
-          '`ActivityId`	integer NOT NULL, '
-          '`PictogramId`	integer NOT NULL, '
-          'PRIMARY KEY(`ActivityId`,`PictogramId`), '
-          'CONSTRAINT `FK_PictogramRelations_Activities_ActivityId` '
-          'FOREIGN KEY(`ActivityId`) '
-          'REFERENCES `Activities`(`Key`) ON DELETE CASCADE, '
-          'CONSTRAINT `FK_PictogramRelations_Pictograms_PictogramId` '
-          'FOREIGN KEY(`PictogramId`) '
-          'REFERENCES `Pictograms`(`id`) ON DELETE CASCADE);');
-      await txn.execute('CREATE TABLE IF NOT EXISTS `Timers` ('
-          '`Key`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
-          '`StartTime`	integer NOT NULL, '
-          '`Progress`	integer NOT NULL, '
-          '`FullLength`	integer NOT NULL, '
-          '`Paused`	integer NOT NULL);');
-    });
+  Future<List<dynamic>> createTables() async {
+    Batch batch = _database.batch();
+    batch.execute('CREATE TABLE IF NOT EXISTS `Users` ('
+        '`Id` varchar( 255 ) NOT NULL, '
+        '`Role` varchar ( 255 ) NOT NULL, '
+        '`RoleName` varchar ( 256 ) DEFAULT NULL, '
+        '`Username` varchar ( 256 ) DEFAULT NULL, '
+        '`DisplayName` longtext NOT NULL, '
+        '`Department` integer DEFAULT NULL, '
+        'UNIQUE(`Id`,`UserName`), '
+        'PRIMARY KEY(`Id`));');
+    batch.execute('CREATE TABLE IF NOT EXISTS `GuardianRelations` ('
+        '`Id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
+        '`CitizenId`	varchar ( 255 ) NOT NULL, '
+        '`GuardianId`	varchar ( 255 ) NOT NULL, '
+        'CONSTRAINT `FK_GuardianRelations_Users_CitizenId` '
+        'FOREIGN KEY(`CitizenId`) '
+        'REFERENCES `AspNetUsers`(`Id`) ON DELETE CASCADE, '
+        'CONSTRAINT `FK_GuardianRelations_Users_GuardianId` '
+        'FOREIGN KEY(`GuardianId`) '
+        'REFERENCES `AspNetUsers`(`Id`) ON DELETE CASCADE);');
+    batch.execute('CREATE TABLE IF NOT EXISTS `WeekTemplates` ('
+        '`id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
+        '`Name`	longtext COLLATE BINARY, '
+        '`ThumbnailKey`	integer NOT NULL, '
+        'CONSTRAINT `FK_WeekTemplates_Pictograms_ThumbnailKey` '
+        'FOREIGN KEY(`ThumbnailKey`) '
+        'REFERENCES `Pictograms`(`id`) ON DELETE CASCADE);');
+    batch.execute('CREATE TABLE IF NOT EXISTS `Weeks` ('
+        '`id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, '
+        '`GirafUserId`	varchar ( 255 ) NOT NULL, '
+        '`Name`	longtext COLLATE BINARY, '
+        '`ThumbnailKey`	integer NOT NULL, '
+        '`WeekNumber`	integer NOT NULL, '
+        '`WeekYear`	integer NOT NULL,'
+        'CONSTRAINT `FK_Weeks_AspNetUsers_GirafUserId` '
+        'FOREIGN KEY(`GirafUserId`) '
+        'REFERENCES `AspNetUsers`(`Id`) ON DELETE CASCADE,'
+        'CONSTRAINT `FK_Weeks_Pictograms_ThumbnailKey` '
+        'FOREIGN KEY(`ThumbnailKey`) '
+        'REFERENCES `Pictograms`(`id`) ON DELETE CASCADE);');
+    batch.execute('CREATE TABLE IF NOT EXISTS `Weekdays` ('
+        '`id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, '
+        '`Day`	integer NOT NULL, '
+        '`WeekId`	integer DEFAULT NULL, '
+        '`WeekTemplateId`	integer DEFAULT NULL,'
+        'CONSTRAINT `FK_Weekdays_WeekTemplates_WeekTemplateId` '
+        'FOREIGN KEY(`WeekTemplateId`) '
+        'REFERENCES `WeekTemplates`(`id`) ON DELETE CASCADE,'
+        'CONSTRAINT `FK_Weekdays_Weeks_WeekId` '
+        'FOREIGN KEY(`WeekId`) '
+        'REFERENCES `Weeks`(`id`) ON DELETE CASCADE);');
+    batch.execute('CREATE TABLE IF NOT EXISTS `Pictograms` ('
+        '`id`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
+        '`AccessLevel`	integer NOT NULL, '
+        '`LastEdit`	datetime ( 6 ) NOT NULL, '
+        '`Sound`	longblob, '
+        '`Title`	varchar ( 255 ) NOT NULL, '
+        '`ImageHash`	longtext COLLATE BINARY,'
+        'UNIQUE(`id`,`Title`));');
+    batch.execute('CREATE TABLE IF NOT EXISTS `Activities` ('
+        '`Key`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
+        '`Order`	integer NOT NULL, '
+        '`OtherKey`	integer NOT NULL, '
+        '`State`	integer NOT NULL, '
+        '`TimerKey`	integer DEFAULT NULL, '
+        '`IsChoiceBoard`	integer NOT NULL DEFAULT \'0\', '
+        'CONSTRAINT `FK_Activities_Timers_TimerKey` '
+        'FOREIGN KEY(`TimerKey`) '
+        'REFERENCES `Timers`(`Key`) ON DELETE SET NULL,'
+        'CONSTRAINT `FK_Activities_Weekdays_OtherKey` '
+        'FOREIGN KEY(`OtherKey`) '
+        'REFERENCES `Weekdays`(`id`) ON DELETE CASCADE);');
+    batch.execute('CREATE TABLE IF NOT EXISTS `PictogramRelations` ('
+        '`ActivityId`	integer NOT NULL, '
+        '`PictogramId`	integer NOT NULL, '
+        'PRIMARY KEY(`ActivityId`,`PictogramId`), '
+        'CONSTRAINT `FK_PictogramRelations_Activities_ActivityId` '
+        'FOREIGN KEY(`ActivityId`) '
+        'REFERENCES `Activities`(`Key`) ON DELETE CASCADE, '
+        'CONSTRAINT `FK_PictogramRelations_Pictograms_PictogramId` '
+        'FOREIGN KEY(`PictogramId`) '
+        'REFERENCES `Pictograms`(`id`) ON DELETE CASCADE);');
+    batch.execute('CREATE TABLE IF NOT EXISTS `Timers` ('
+        '`Key`	integer NOT NULL PRIMARY KEY AUTOINCREMENT, '
+        '`StartTime`	integer NOT NULL, '
+        '`Progress`	integer NOT NULL, '
+        '`FullLength`	integer NOT NULL, '
+        '`Paused`	integer NOT NULL);');
+    final List<dynamic> res = await batch.commit();
+    return res;
   }
 
-  Future<void> closeDb() async {
-    final Database database = await _database;
-    await database.close();
+  Future<void> closeDb() {
+    _database.close();
   }
 }
--- test/database/database_creation_test.dart
+++ test/database/database_creation_test.dart
@@ -1,31 +1,23 @@
 import 'dart:io';
 
 import 'package:api_client/offline_database/offline_db_handler.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-class OfflineDbHandlerMock extends OfflineDbHandler {
-  @override
-  Future<Database> initDB() async {
-    return databaseFactoryFfi
-        .openDatabase(join(Directory.current.path, 'giraf.db'));
-  }
-}
-
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  OfflineDbHandlerMock testDb = OfflineDbHandlerMock();
+  sqfliteFfiInit();
   test('Try to create the test db', () async {
-    testDb.createTables();
+    Database test = await databaseFactoryFfi
+        .openDatabase(join(Directory.current.path, 'girafTest.db'));
+    OfflineDbHandler testDb = OfflineDbHandler(test);
+
     if (testDb == null) {
       print('WHYYYYYYYYYYYYYY');
     }
+    await testDb.createTables();
     expect('true', 'true');
-    await testDb.closeDb();
+    //testDb.closeDb();
   });
 }
