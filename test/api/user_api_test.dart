30e2bb0e9d8fc7c7299f627b451a19fae898abba
Simon <58163499+Sijmon@users.noreply.github.com>
2021-12-03 11:46:08 +0100

    Update (UserApiTest): Fixed tests to support the offline db

--- test/api/user_api_test.dart
+++ test/api/user_api_test.dart
@@ -17,13 +17,13 @@ import 'package:api_client/http/http_mock.dart';
 import 'package:api_client/models/giraf_user_model.dart';
 import 'package:api_client/models/settings_model.dart';
 import 'package:api_client/models/displayname_model.dart';
-import 'package:api_client/offline_database/offline_db_handler.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-import '../database/Offline_models.dart';
+import '../database/OfflineDatabase_test.dart';
+
 class ConnectivityMock implements Connectivity {
   bool isConnected = true;
 
@@ -43,6 +43,7 @@ class ConnectivityMock implements Connectivity {
 Future<void> main() async {
   ConnectivityMock connectivityMock;
   sqfliteFfiInit();
+  MockOfflineDbHandler dbHandlerMock;
   UserApi userApi;
   HttpMock httpMock;
 
@@ -73,8 +74,12 @@ Future<void> main() async {
   setUp(() {
     httpMock = HttpMock();
     connectivityMock = ConnectivityMock();
-    userApi = UserApi(httpMock, ConnectivityApi
-                .withConnectivity(StatusApi(httpMock), connectivityMock));
+    dbHandlerMock = MockOfflineDbHandler.instance;
+    userApi = UserApi.withMockDbHandler(
+      httpMock,
+      ConnectivityApi.withConnectivity(StatusApi(httpMock), connectivityMock),
+      dbHandlerMock
+    );
   });
 
   test('Should fetch authenticated user from online', () async {
@@ -105,7 +110,7 @@ Future<void> main() async {
   test('Should fetch authenticated user from offline', () {
     connectivityMock.isConnected = false;
 
-    OfflineDbHandler.instance.setMe(user);
+    dbHandlerMock.setMe(user);
 
     userApi.me().listen(expectAsync1((GirafUserModel authUser) {
       expect(authUser.toJson(), user.toJson());
@@ -127,18 +132,32 @@ Future<void> main() async {
     'errorKey': 'NoError'
     });
 
-    httpMock
-        .expectOne(url: '/${user.id}', method: Method.get)
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    httpMock.expectOne(url: '/${user.id}', method: Method.get)
         .flush(<String, dynamic>{
       'data': user.toJson(),
       'message': '',
       'errorKey': 'NoError',
     });
   });
-  test('Should get the role endpoint', () {
+
+  test('Should get the role endpoint', () async {
     userApi.role(user.username).listen(expectAsync1((int roleIndex) {
       expect(roleIndex, user.role.index);
     }));
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    // This is expecting a call to the status api (on status())
+    httpMock.expectOne(url: '/', method: Method.get).flush(<String, dynamic>{
+    'data': true,
+    'success': true,
+    'message': '',
+    'errorKey': 'NoError'
+    });
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
 
     httpMock
         .expectOne(url: '/${user.username}/role', method: Method.get)
@@ -149,10 +168,22 @@ Future<void> main() async {
     });
   });
 
-  test('Should update user with ID', () {
+  test('Should update user with ID', () async {
     userApi.update(user).listen(expectAsync1((GirafUserModel specUser) {
       expect(specUser.toJson(), user.toJson());
     }));
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    // This is expecting a call to the status api (on status())
+    httpMock.expectOne(url: '/', method: Method.get).flush(<String, dynamic>{
+    'data': true,
+    'success': true,
+    'message': '',
+    'errorKey': 'NoError'
+    });
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
 
     httpMock
         .expectOne(url: '/${user.id}', method: Method.put)
@@ -163,12 +194,24 @@ Future<void> main() async {
     });
   });
 
-  test('Should get settings from user with ID', () {
+  test('Should get settings from user with ID', () async {
     userApi
         .getSettings(user.id)
         .listen(expectAsync1((SettingsModel specSettings) {
       expect(specSettings.toJson(), settings.toJson());
     }));
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    // This is expecting a call to the status api (on status())
+    httpMock.expectOne(url: '/', method: Method.get).flush(<String, dynamic>{
+    'data': true,
+    'success': true,
+    'message': '',
+    'errorKey': 'NoError'
+    });
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
 
     httpMock
         .expectOne(url: '/${user.id}/settings', method: Method.get)
@@ -179,7 +222,7 @@ Future<void> main() async {
     });
   });
 
-  test('Should get an error when getting settings from user with ID', () {
+  test('Should get an error when getting settings from user with ID', () async {
     userApi.getSettings(user.id).listen((_) {},
         onError: expectAsync1((ApiException error) {
       expect(error.errorKey, ErrorKey.RoleMustBeCitizien);
@@ -191,18 +234,42 @@ Future<void> main() async {
       'errorKey': 'RoleMustBeCitizien',
     };
 
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    // This is expecting a call to the status api (on status())
+    httpMock.expectOne(url: '/', method: Method.get).flush(<String, dynamic>{
+    'data': true,
+    'success': true,
+    'message': '',
+    'errorKey': 'NoError'
+    });
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
     httpMock
         .expectOne(
             url: '/${user.id}/settings', method: Method.get, statusCode: 400)
         .flush(Response(http.Response(jsonEncode(body), 400), body));
   });
 
-  test('Should update settings from user with ID', () {
+  test('Should update settings from user with ID', () async {
     userApi
         .updateSettings(user.id, settings)
         .listen(expectAsync1((SettingsModel specSettings) {
       expect(specSettings.toJson(), settings.toJson());
     }));
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    // This is expecting a call to the status api (on status())
+    httpMock.expectOne(url: '/', method: Method.get).flush(<String, dynamic>{
+    'data': true,
+    'success': true,
+    'message': '',
+    'errorKey': 'NoError'
+    });
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
 
     httpMock
         .expectOne(url: '/${user.id}/settings', method: Method.put)
@@ -213,7 +280,7 @@ Future<void> main() async {
     });
   });
 
-  test('Should get an error when updating settings from user with ID', () {
+  test('Should get an error when updating settings from user with ID', ()async {
     userApi.updateSettings(user.id, settings).listen((_) {},
         onError: expectAsync1((ApiException error) {
       expect(error.errorKey, ErrorKey.RoleMustBeCitizien);
@@ -224,19 +291,43 @@ Future<void> main() async {
       'errorKey': 'RoleMustBeCitizien',
     };
 
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    // This is expecting a call to the status api (on status())
+    httpMock.expectOne(url: '/', method: Method.get).flush(<String, dynamic>{
+      'data': true,
+      'success': true,
+      'message': '',
+      'errorKey': 'NoError'
+    });
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
     httpMock
         .expectOne(
             url: '/${user.id}/settings', method: Method.put, statusCode: 400)
         .flush(Response(http.Response(jsonEncode(body), 400), body));
   });
 
-  test('Should get citizens from user with ID', () {
+  test('Should get citizens from user with ID', () async {
     userApi
         .getCitizens(user.id)
         .listen(expectAsync1((List<DisplayNameModel> names) {
       expect(names.map((DisplayNameModel name) => name.toJson()),
           usernames.map((DisplayNameModel name) => name.toJson()));
     }));
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    // This is expecting a call to the status api (on status())
+    httpMock.expectOne(url: '/', method: Method.get).flush(<String, dynamic>{
+      'data': true,
+      'success': true,
+      'message': '',
+      'errorKey': 'NoError'
+    });
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
 
     httpMock
         .expectOne(url: '/${user.id}/citizens', method: Method.get)
@@ -247,13 +338,25 @@ Future<void> main() async {
     });
   });
 
-  test('Should get citizens from user with ID', () {
+  test('Should get citizens from user with ID', () async {
     userApi
         .getGuardians(user.id)
         .listen(expectAsync1((List<DisplayNameModel> names) {
       expect(names.map((DisplayNameModel name) => name.toJson()),
           usernames.map((DisplayNameModel name) => name.toJson()));
     }));
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    // This is expecting a call to the status api (on status())
+    httpMock.expectOne(url: '/', method: Method.get).flush(<String, dynamic>{
+      'data': true,
+      'success': true,
+      'message': '',
+      'errorKey': 'NoError'
+    });
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
 
     httpMock
         .expectOne(url: '/${user.id}/guardians', method: Method.get)
@@ -264,14 +367,25 @@ Future<void> main() async {
     });
   });
 
-  test('Should get citizens from user with ID', () {
+  test('Should get citizens from user with ID', () async {
     const String citizenId = '1234';
 
-    userApi
-        .addCitizenToGuardian(user.id, citizenId)
+    userApi.addCitizenToGuardian(user.id, citizenId)
         .listen(expectAsync1((bool success) {
       expect(success, isTrue);
     }));
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
+
+    // This is expecting a call to the status api (on status())
+    httpMock.expectOne(url: '/', method: Method.get).flush(<String, dynamic>{
+      'data': true,
+      'success': true,
+      'message': '',
+      'errorKey': 'NoError'
+    });
+
+    await Future<dynamic>.delayed(const Duration(seconds: 1));
 
     httpMock
         .expectOne(url: '/${user.id}/citizens/$citizenId', method: Method.post)
