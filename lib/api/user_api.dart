0f4c6c301b71fd84ec0d8d04c5a219ac42b57e69
Simon <58163499+Sijmon@users.noreply.github.com>
2021-12-09 11:53:51 +0100

    test

--- lib/api/user_api.dart
+++ lib/api/user_api.dart
@@ -89,10 +89,12 @@ class UserApi {
               .fromJson(res.json['data'])).first;
 
         // This will save the settings and update the settingsId for the user
-        settings.then((SettingsModel settings) =>
+        /// Does not work properly, needs to be fixed
+        /// (Says no Settings table exists)
+        /*settings.then((SettingsModel settings) =>
         // TODO(MathiasNielsen): Only insert if the settings does not exist DB
         _dbHandler.insertUserSettings(id, settings)
-        );
+        );*/
 
         return settings;
       },
