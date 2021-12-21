9ab17463bcc458212e1af2fa66cec2e9f7278f41
Casper Søgård <casper-s@hotmail.dk>
2021-12-21 21:03:13 +0100

    Feature/817 (#101)
    * new trustee relation
    
    new trustee relation functionality
    
    * Removed curly bracket
    
    * Updatet comment
    
    Co-authored-by: Nima Moayedzadeh <57714574+Nima-Moayedzadeh@users.noreply.github.com>

--- lib/api/user_api.dart
+++ lib/api/user_api.dart
@@ -166,4 +166,15 @@ class UserApi {
   Stream<bool> addCitizenToGuardian(String guardianId, String citizenId) =>
       _http.post('/$guardianId/citizens/$citizenId')
           .map((Response res) => res.statusCode() == 200);
+
+
+/// Adds relation between the authenticated user (trustee) and an
+  /// existing citizen.
+  ///
+  /// [trusteeId] The trustee
+  /// [citizenId] The citizen to be added to the guardian
+  ///
+  Stream<bool> addCitizenToTrustee(String trusteeId, String citizenId) =>
+      _http.post('/$trusteeId/citizens/$citizenId')
+          .map((Response res) => res.statusCode() == 200);
 }
