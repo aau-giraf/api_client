542aa61d7e5a5c8a05186d31e64c002ca001271c
Makel <mikkel@dissing.one>
2022-10-11 14:32:11 +0200

    showpopup removed
    dosent exist in the model

--- test/models/settings_model_test.dart
+++ test/models/settings_model_test.dart
@@ -16,10 +16,9 @@ void main() {
     'activitiesCount': null,
     'theme': 1,
     'nrOfDaysToDisplay': 7,
-    'lockTimerControl': false,
     'greyScale': false,
     'pictogramText' : false,
-    'showPopup' : false,
+    'lockTimerControl': false,
     'weekDayColors': <dynamic>[
       <String, dynamic>{'hexColor': '#067700', 'day': 1},
       <String, dynamic>{'hexColor': '#8c1086', 'day': 2},
