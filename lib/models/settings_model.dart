4fe61f39981c573ed7db38433440b45fe5cf5681
Lildhansen <madslh@yahoo.com>
2022-10-28 08:44:20 +0200

    Revert "changes necessary to solve issue #571 in weekplanner (https://github.com/aau-giraf/weekplanner/issues/571) (#112)"
    This reverts commit f9d026da5344ca264069568962f855987a84cd2b.

--- lib/models/settings_model.dart
+++ lib/models/settings_model.dart
@@ -18,10 +18,7 @@ class SettingsModel implements Model {
       this.timerSeconds,
       this.activitiesCount,
       @required this.theme,
-      this.nrOfDaysToDisplayPortrait,
-      this.displayDaysRelativePortrait,
-      this.nrOfDaysToDisplayLandscape,
-      this.displayDaysRelativeLandscape,
+      this.nrOfDaysToDisplay,
       this.greyscale,
       this.lockTimerControl,
       this.pictogramText,
@@ -42,10 +39,7 @@ class SettingsModel implements Model {
     timerSeconds = json['timerSeconds'];
     activitiesCount = json['activitiesCount'];
     theme = GirafTheme.values[(json['theme']) - 1];
-    nrOfDaysToDisplayPortrait = json['nrOfDaysToDisplayPortrait'];
-    displayDaysRelativePortrait = json['displayDaysRelativePortrait'];
-    nrOfDaysToDisplayLandscape = json['nrOfDaysToDisplayLandscape'];
-    displayDaysRelativeLandscape = json['displayDaysRelativeLandscape'];
+    nrOfDaysToDisplay = json['nrOfDaysToDisplay'];
     greyscale = json['greyScale'];
     lockTimerControl = json['lockTimerControl'];
     pictogramText = json['pictogramText'];
@@ -60,7 +54,7 @@ class SettingsModel implements Model {
     }
   }
 
-  /// Create a Settingsmodel from json from the database
+  /// Create a Settingsmodel from jason from the database
   SettingsModel.fromDatabase(Map<String, dynamic> settingsJson,
       List<Map<String, dynamic>> weekdayColorsJson) {
     if (settingsJson == null) {
@@ -75,14 +69,7 @@ class SettingsModel implements Model {
     timerSeconds = settingsJson['timerSeconds'];
     activitiesCount = settingsJson['activitiesCount'];
     theme = GirafTheme.values[(settingsJson['theme'])];
-    nrOfDaysToDisplayPortrait =
-    settingsJson['nrOfDaysToDisplayPortrait'];
-    displayDaysRelativePortrait =
-    settingsJson['displayDaysRelativePortrait'];
-    nrOfDaysToDisplayLandscape =
-    settingsJson['nrOfDaysToDisplayLandscape'];
-    displayDaysRelativeLandscape =
-    settingsJson['displayDaysRelativeLandscape'];
+    nrOfDaysToDisplay = settingsJson['nrOfDaysToDisplay'];
     greyscale = settingsJson['greyScale'] == 1;
     lockTimerControl = settingsJson['lockTimerControl'] == 1;
     pictogramText = settingsJson['pictogramText'] == 1;
@@ -118,25 +105,8 @@ class SettingsModel implements Model {
   /// The preferred theme
   GirafTheme theme;
 
-  /// Defines the number of days to display in portrait mode
-  /// for a user in a weekplan
-  int nrOfDaysToDisplayPortrait;
-
-  /// true: if the first day shown in the weekplanner in portrait mode
-  /// should be today
-  /// false: if the first day shown in the weekplanner in portrait mode
-  /// should be monday
-  bool displayDaysRelativePortrait;
-
-  /// Defines the number of days to display in landscape mode
-  /// for a user in a weekplan
-  int nrOfDaysToDisplayLandscape;
-
-  /// true: if the first day shown in the weekplanner in landscape mode
-  /// should be today
-  /// false: if the first day shown in the weekplanner in landscape mode
-  /// should be monday
-  bool displayDaysRelativeLandscape;
+  /// defines the number of days to display for a user in a weekschedule
+  int nrOfDaysToDisplay;
 
   /// Flag for indicating whether or not greyscale is enabled
   bool greyscale;
@@ -147,7 +117,7 @@ class SettingsModel implements Model {
   /// Defines if text should be shown alongside the pictograms in the weekplan
   bool pictogramText;
 
-  /// Defines if a popup should be shown on the choice board and activity timers
+  // Defines if a popup should be shown on the choice board and activity timers
   bool showPopup;
 
   /// List of weekday colors shown in the weekplan
@@ -163,10 +133,7 @@ class SettingsModel implements Model {
       'timerSeconds': timerSeconds,
       'activitiesCount': activitiesCount,
       'theme': theme.index + 1,
-      'nrOfDaysToDisplayPortrait': nrOfDaysToDisplayPortrait,
-      'displayDaysRelativePortrait': displayDaysRelativePortrait,
-      'nrOfDaysToDisplayLandscape': nrOfDaysToDisplayLandscape,
-      'displayDaysRelativeLandscape': displayDaysRelativeLandscape,
+      'nrOfDaysToDisplay': nrOfDaysToDisplay,
       'greyScale': greyscale,
       'lockTimerControl': lockTimerControl,
       'pictogramText': pictogramText,
