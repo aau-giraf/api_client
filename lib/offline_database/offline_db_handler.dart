c97c408772b3b9a171b4b28a55c11725f491bbef
Chris31415926 <75470812+Chris31415926@users.noreply.github.com>
2022-11-04 10:14:24 +0100

    Comented out some more values due to an error

--- lib/offline_database/offline_db_handler.dart
+++ lib/offline_database/offline_db_handler.dart
@@ -477,7 +477,7 @@ class OfflineDbHandler {
       final int settingsId = await db.rawInsert('''INSERT INTO SETTINGS
         (orientation, completeMark, cancelMark, defaultTimer, timerSeconds,
         activitiesCount, theme, nrOfDaysToDisplay, greyScale, lockTimerControl,
-        pictogramText, nrOfActivitiesToDisplay, showOnlyActivities) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+        pictogramText) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
         <dynamic>[
           settings.orientation.index,
           settings.completeMark.index,
@@ -491,8 +491,8 @@ class OfflineDbHandler {
           settings.lockTimerControl,
           settings.pictogramText,
           ///settings.showPopup,
-          settings.nrOfActivitiesToDisplay,
-          settings.showOnlyActivities]);
+          ///settings.nrOfActivitiesToDisplay,
+          ///settings.showOnlyActivities]);
 
 
       await db.rawUpdate(
