2eff5e8fccb891c657f2e194a8447c87775c1b77
Racermus01 <73647518+Bisgaard01@users.noreply.github.com>
2022-11-16 14:34:11 +0100

    Refactored persistence test + added comment saying macos fix is a macos fix

--- test/persistence/persistence_test.dart
+++ test/persistence/persistence_test.dart
@@ -5,49 +5,40 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  test('Should store', () async {
-    SharedPreferences.setMockInitialValues(<String, Object>{});
-    WidgetsFlutterBinding.ensureInitialized();
-    //The old package is deprecated so we had to write it ourselves
-    TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger
-        ?.setMockMethodCallHandler(
-      const MethodChannel('plugins.flutter.io/shared_preferences'),
-    (MethodCall methodCall) async {
-    if (methodCall.method == 'getAll') {
-    return <String, dynamic>{}; // set initial values here if desired
-    }
+  void mockMethodCallHandler(){
+  //Necessary for tests to pass on macos
+  SharedPreferences.setMockInitialValues(<String, Object>{});
 
-    if (methodCall.method == 'setString') {
-    // pass here
-    return null;
-    }
-    fail('Should call setString');
-    });
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //The old package is deprecated so we had to write it ourselves
+  TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger
+      ?.setMockMethodCallHandler(
+  const MethodChannel('plugins.flutter.io/shared_preferences'),
+  (MethodCall methodCall) async {
+  if (methodCall.method == 'getAll') {
+  return <String, dynamic>{}; // set initial values here if desired
+  }
+
+  if (methodCall.method == 'setString') {
+  // pass here
+  return null;
+  }
+  fail('Should call setString');
+  });
+  }
+
+  test('Should store', () async {
+    mockMethodCallHandler();
 
     const String token = 'Test Token';
     await PersistenceClient().set('token', token);
   });
 
   test('Should get token', () async {
-    SharedPreferences.setMockInitialValues(<String, Object>{});
-    WidgetsFlutterBinding.ensureInitialized();
-
-    //The old package is deprecated so we had to write it ourselves
-    TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger
-        ?.setMockMethodCallHandler(
-        const MethodChannel('plugins.flutter.io/shared_preferences'),
-            (MethodCall methodCall) async {
-          if (methodCall.method == 'getAll') {
-            return <String, dynamic>{}; // set initial values here if desired
-          }
-
-          if (methodCall.method == 'setString') {
-            // pass here
-            return null;
-          }
-          fail('Should call setString');
-        });
+    mockMethodCallHandler();
 
     await PersistenceClient().get('token');
   });
 }
+
