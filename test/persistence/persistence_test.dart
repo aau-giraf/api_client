ce74b80b5d6f259c9294bf02e99e6ed740484099
Racermus01 <73647518+Bisgaard01@users.noreply.github.com>
2022-11-16 11:13:41 +0100

    Potential macos fix

--- test/persistence/persistence_test.dart
+++ test/persistence/persistence_test.dart
@@ -2,11 +2,13 @@ import 'package:api_client/persistence/persistence_client.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   test('Should store', () async {
+    SharedPreferences.setMockInitialValues({});
     WidgetsFlutterBinding.ensureInitialized();
-    //The old package is deprecated so we had to write it ourselves 
+    //The old package is deprecated so we had to write it ourselves
     TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger
         ?.setMockMethodCallHandler(
       const MethodChannel('plugins.flutter.io/shared_preferences'),
@@ -27,6 +29,7 @@ void main() {
   });
 
   test('Should get token', () async {
+    SharedPreferences.setMockInitialValues({});
     WidgetsFlutterBinding.ensureInitialized();
 
     //The old package is deprecated so we had to write it ourselves
