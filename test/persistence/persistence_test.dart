873c724ac7e68e9769e4570b9e68dad1197ea543
Jacob Søndergaard <jacob@cavas.dk>
2023-10-26 13:35:32 +0200

    Remove unused import

--- test/persistence/persistence_test.dart
+++ test/persistence/persistence_test.dart
@@ -1,5 +1,4 @@
 import 'package:api_client/persistence/persistence_client.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
